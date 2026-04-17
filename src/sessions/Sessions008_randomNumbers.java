dd51ced1676c53e3e162c6de50d49099570841f7
Camille Sun <camille.sun@MacBook-Air-Camille-Sun.local>
2026-04-17 23:59:00 +0100

    adds printf

--- src/sessions/Sessions008_randomNumbers.java
+++ src/sessions/Sessions008_randomNumbers.java
@@ -3,26 +3,24 @@ package sessions;
 import java.util.Random;
 
 public class Sessions008_randomNumbers {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Random random = new Random();
         int number;
         int number1;
         double number2;
-        number = random.nextInt(0,8);
+        number = random.nextInt(0, 8);
         number1 = random.nextInt(0, 7);
         number2 = random.nextInt(0, 7);
         System.out.println(number);
         System.out.println(number1);
         System.out.println(number2);
 
-
         boolean isHeads;
         isHeads = random.nextBoolean();
-        if(isHeads){
+        if (isHeads) {
             System.out.println("Heads");
-        }
-        else{
+        } else {
             System.out.println("Tails");
         }
 
