06522ed52b495be07c3d539d499947a0d8e83855
Camille Sun <camille.sun@MacBook-Air-Camille-Sun.local>
2026-04-20 22:57:13 +0100

    adds calculation of compounded interest

--- src/sessions/Sessions011_compoundInterestRate.java
+++ src/sessions/Sessions011_compoundInterestRate.java
@@ -1,4 +1,32 @@
 package sessions;
 
+import java.util.Scanner;
+
 public class Sessions011_compoundInterestRate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double principal;
+        double rate;
+        int timesCompound;
+        int years;
+        double amount;
+
+        System.out.print("Please enter the principle amount:  ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Please enter the interest rate:  ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Please enter the # of times compounded per year:  ");
+        timesCompound = scanner.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompound, timesCompound * years);
+        System.out.printf("The amount after %d years is $%.2f", years, amount);
+
+        scanner.close();
+
+    }
 }
